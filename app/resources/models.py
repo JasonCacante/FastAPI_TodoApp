fe9920a1465216f53d8af0cdeb06a88f26718a0b
JasonCacante <jason.cacante@gmail.com>
2025-06-09 08:27:09 -0500

    Add Users model and establish relationship with Todos model

--- app/resources/models.py
+++ app/resources/models.py
@@ -1,5 +1,21 @@
 from resources.db import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username={self.username})>"
 
 
 class Todos(Base):
@@ -10,6 +26,7 @@ class Todos(Base):
     description = Column(String)
     completed = Column(Boolean, default=False)  # Default completed status set to False
     priority = Column(Integer, default=1)  # Default priority set to 1
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
     def __repr__(self):
         return f"<Todo(id={self.id}, title={self.title}, completed={self.completed})>"
