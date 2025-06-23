7e7c78eb03dafe140987ca2076bf8d3808b00739
JasonCacante <jason.cacante@gmail.com>
2025-06-22 22:29:39 -0500

    Add admin and users routers to main application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from resources.models import Base, Todos
 from resources.db import engine
-from resources.routers import auth, todos
+from resources.routers import auth, todos, admin, users
 
 app = FastAPI()
 
@@ -9,3 +9,5 @@ Base.metadata.create_all(bind=engine)  # Create database tables
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
