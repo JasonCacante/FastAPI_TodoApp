010637fa731e4aa9ed1a38a3d31e080dc262ef5f
JasonCacante <jason.cacante@gmail.com>
2025-07-04 07:35:16 -0500

    Add tests for Student class and its attributes

--- app/resources/test/test_example.py
+++ app/resources/test/test_example.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_equal_or_not_equal():
     assert 1 == 1
     assert 2 != 3
@@ -31,3 +34,35 @@ def test_list():
     assert my_list[1] != 4
     assert 1 in my_list
     assert 7 not in my_list
+
+
+class Student:
+    def __init__(self, first_name: str, last_name: str, major: str, years: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+
+@pytest.fixture
+def default_student():
+    return Student("John", "Doe", "Computer Science", 3)
+
+
+def test_student_class(default_student):
+    assert default_student.first_name == "John"
+    assert default_student.last_name == "Doe"
+    assert default_student.major == "Computer Science"
+    assert default_student.years == 3
+
+    # Test attributes
+    assert hasattr(default_student, "first_name")
+    assert hasattr(default_student, "last_name")
+    assert hasattr(default_student, "major")
+    assert hasattr(default_student, "years")
+
+    # Test attribute types
+    assert isinstance(default_student.first_name, str)
+    assert isinstance(default_student.last_name, str)
+    assert isinstance(default_student.major, str)
+    assert isinstance(default_student.years, int)
